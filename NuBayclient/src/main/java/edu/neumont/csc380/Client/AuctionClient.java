c2acf23f7eed4dfd899e3c2dee10bf4146fa2444
timonium694 <ttoth@student.neumont.edu>
2014-05-08 20:37:19 -0600

    this is the working version to tuen in and stuff

--- NuBayclient/src/main/java/edu/neumont/csc380/Client/AuctionClient.java
+++ NuBayclient/src/main/java/edu/neumont/csc380/Client/AuctionClient.java
@@ -1,21 +1,35 @@
 package edu.neumont.csc380.Client;
 
 import edu.neumont.csc380.AuctionInterfaces.IAuction;
+import edu.neumont.csc380.ProfileWebService.model.*;
+import edu.neumont.csc380.ProfileWebService.services.ProfileClient;
 import edu.neumont.csc380.auth.Authorization.AuthorityLevel;
 import edu.neumont.csc380.auth.client.AuthClientImpl;
+
 
 public class AuctionClient {
 	
 	public static void main(String[] args)
 	{
 		AuthClientImpl ai = new AuthClientImpl();
+		ProfileClient pi = new ProfileClient();
 		try
 		{
-			ai.createUser("tim", "isnotgay", AuthorityLevel.Admin);
-			ai.authenticateUser("tim", "isnotgay");
-			ai.deleteUser("tim");
-			ai.createUser("adam", "pimpdaddyflex", AuthorityLevel.User);
-			ai.updateUser("adam", "cthulu", "pimpdaddyflex", AuthorityLevel.Admin);
+			Profile p = new Profile();
+			p.setUsername("test");
+			p.setPassword("password");
+			p.setLastName("last");
+			p.setFirstName("first");
+			ContactInformation c = new ContactInformation();
+			c.setAddress("here");
+			c.setEmail("this@email.com");
+			c.setPhone("012394");
+			p.setContactInformation(c);
+
+			ai.createUser(p.getUsername(), p.getPassword(), AuthorityLevel.Admin);
+			ai.authenticateUser(p.getUsername(), p.getPassword());
+			pi.createProfile(p);
+			
 		}
 		catch(Exception e)
 		{
